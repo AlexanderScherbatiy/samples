4507c40076a43f0dea0f8f111067fe587adc4dda
AlexanderScherbatiy <alexander.scherbatiy@yandex.com>
2017-01-02 00:16:54 +0300

    Use ordered type in the Scala BinarySearchTree sample

--- scala/datatype/src/datatype/bst/BinarySearchTree.scala
+++ scala/datatype/src/datatype/bst/BinarySearchTree.scala
@@ -4,32 +4,34 @@ package datatype.bst
   * Created by alexsch.
   */
 
-sealed abstract class BinarySearchTree {
+sealed abstract class BinarySearchTree[T <% Ordered[T]] {
 
-  def insert(value: Double): BinarySearchTree = this match {
-    case EmptyBinarySearchTree => new NonEmptyBinarySeacrhTree(value, EmptyBinarySearchTree, EmptyBinarySearchTree)
+  def insert(value: T): BinarySearchTree[T] = this match {
+    case EmptyBinarySearchTree() => new NonEmptyBinarySeacrhTree[T](value, new EmptyBinarySearchTree(), new EmptyBinarySearchTree())
     case NonEmptyBinarySeacrhTree(v, left, right) =>
       if (value < v) new NonEmptyBinarySeacrhTree(v, left.insert(value), right)
       else if (value > v) new NonEmptyBinarySeacrhTree(v, left, right.insert(value))
       else this
   }
 
+
   override def toString: String = this match {
-    case EmptyBinarySearchTree => ""
+    case EmptyBinarySearchTree() => ""
     case NonEmptyBinarySeacrhTree(v, left, right) => s"($v $left $right)"
   }
 }
 
 
-object BinarySearchTree extends BinarySearchTree {
+object BinarySearchTree extends BinarySearchTree[Nothing] {
 
-  def apply(values: Double*): BinarySearchTree = {
-    values.foldLeft[BinarySearchTree](EmptyBinarySearchTree)((t, value) => t.insert(value))
+  def apply[T <% Ordered[T]](values: T*): BinarySearchTree[T] = {
+    values.foldLeft[BinarySearchTree[T]](new EmptyBinarySearchTree[T]())((t, value) => t.insert(value))
   }
 }
 
-case object EmptyBinarySearchTree extends BinarySearchTree {
+case class EmptyBinarySearchTree[T <% Ordered[T]]() extends BinarySearchTree[T] {
 }
 
-case class NonEmptyBinarySeacrhTree[T](value: Double, left: BinarySearchTree, right: BinarySearchTree) extends BinarySearchTree {
+case class NonEmptyBinarySeacrhTree[T <% Ordered[T]](value: T, left: BinarySearchTree[T], right: BinarySearchTree[T])
+  extends BinarySearchTree[T] {
 }
