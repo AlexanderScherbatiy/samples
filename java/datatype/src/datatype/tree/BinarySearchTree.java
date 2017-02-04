07c51aea88f7720b892a0136b26261a95e71e2ed
AlexanderScherbatiy <alexander.scherbatiy@yandex.com>
2017-02-04 11:32:58 +0300

    Java datat type make BinarySearchTree iterable

--- java/datatype/src/datatype/tree/BinarySearchTree.java
+++ java/datatype/src/datatype/tree/BinarySearchTree.java
@@ -1,12 +1,20 @@
 package datatype.tree;
 
+import com.sun.tools.doclint.Entity;
+import datatype.queue.LinkedQueueDataType;
+import datatype.queue.QueueDataType;
+
+import java.util.Iterator;
 import java.util.Objects;
+import java.util.Queue;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 /**
  * Created by alexsch on 2/3/2017.
  */
 
-public class BinarySearchTree<E extends Comparable<E>> {
+public class BinarySearchTree<E extends Comparable<E>> implements Iterable<E> {
 
     private Node root;
 
@@ -29,6 +37,42 @@ public class BinarySearchTree<E extends Comparable<E>> {
             node.value = value;
             return node;
         }
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+
+        final QueueDataType<Node<E>> queue = new LinkedQueueDataType<>();
+        queue.enqueue(root);
+
+        return new Iterator<E>() {
+
+            @Override
+            public boolean hasNext() {
+                return !queue.isEmpty();
+            }
+
+            @Override
+            public E next() {
+                Node<E> elem = queue.dequeue();
+                Node<E> left = elem.left;
+                Node<E> right = elem.right;
+
+                if (left != null) {
+                    queue.enqueue(left);
+                }
+
+                if (right != null) {
+                    queue.enqueue(right);
+                }
+
+                return elem.value;
+            }
+        };
+    }
+
+    public Stream<E> stream() {
+        return StreamSupport.stream(spliterator(), false);
     }
 
     @Override
--- java/datatype/test/datatype/BinarySearchTreeTest.java
+++ java/datatype/test/datatype/BinarySearchTreeTest.java
@@ -2,18 +2,51 @@ package datatype;
 
 import datatype.tree.BinarySearchTree;
 
+import java.util.stream.Stream;
+
 /**
  * Created by alexsch on 2/3/2017.
  */
 public class BinarySearchTreeTest {
 
     public static void main(String[] args) {
+        testTree();
+        testIterator();
+        testStream();
+    }
 
+    private static void testTree() {
         BinarySearchTree<Integer> tree = new BinarySearchTree<>();
         tree.insert(2);
         tree.insert(3);
         tree.insert(1);
 
-        System.out.printf("tree: %s", tree);
+        System.out.printf("tree: %s\n", tree);
+    }
+
+    private static void testIterator() {
+        BinarySearchTree<Integer> tree = new BinarySearchTree<>();
+        tree.insert(5);
+        tree.insert(6);
+        tree.insert(4);
+
+        System.out.printf("tree: %s\n", tree);
+
+        for (Integer value : tree) {
+            System.out.printf("iterator next value: %d\n", value);
+        }
+    }
+
+    private static void testStream() {
+        BinarySearchTree<Integer> tree = new BinarySearchTree<>();
+        tree.insert(9);
+        tree.insert(7);
+        tree.insert(8);
+
+        System.out.printf("tree: %s\n", tree);
+
+        tree.stream().forEach((elem) -> {
+            System.out.printf("foreach: %s\n", elem);
+        });
     }
 }
