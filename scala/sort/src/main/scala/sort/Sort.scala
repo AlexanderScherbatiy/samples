53f0493352c9dd7c75114ab34a8a573b93d3526d
AlexanderScherbatiy <alexander.scherbatiy@yandex.com>
2017-08-22 15:48:03 +0300

    Add Scala Selection sort

--- scala/sort/src/main/scala/sort/Sort.scala
+++ scala/sort/src/main/scala/sort/Sort.scala
@@ -2,6 +2,34 @@ package sort
 
 object Sort {
 
+  def selectionSort[A <% Ordered[A]](list: List[A]) = {
+
+    def extractMax(max: A, l: List[A]): (A, List[A]) = {
+      l match {
+        case Nil => (max, Nil)
+        case head :: tail => if (max < head) {
+          val (m, rest) = extractMax(head, tail)
+          (m, max :: rest)
+        } else {
+          val (m, rest) = extractMax(max, tail)
+          (m, head :: rest)
+        }
+      }
+    }
+
+
+    def sort(l: List[A], acc: List[A]): List[A] = {
+      l match {
+        case Nil => acc
+        case head :: tail =>
+          val (max, rest) = extractMax(head, tail)
+          sort(rest, max :: acc)
+      }
+    }
+
+    if (list.isEmpty) Nil else sort(list, Nil)
+  }
+
   def insertSort[A <% Ordered[A]](list: List[A]) = {
 
     def insert(value: A, list: List[A]): List[A] = list match {
--- scala/sort/src/test/scala/sort/SortTest.scala
+++ scala/sort/src/test/scala/sort/SortTest.scala
@@ -1,7 +1,7 @@
 package sort
 
 import org.scalatest.FunSuite
-import sort.Sort.insertSort
+import sort.Sort._
 
 import scala.util.Random
 
@@ -15,14 +15,33 @@ class SortTest extends FunSuite {
     assert(insertSort(1 :: Nil) === 1 :: Nil)
   }
 
+  test("Selection Sort 1 element") {
+    assert(selectionSort(1 :: Nil) === 1 :: Nil)
+  }
+
+
   test("Insert Sort 2 elements") {
     assert(insertSort(1 :: 2 :: Nil) === 1 :: 2 :: Nil)
     assert(insertSort(2 :: 1 :: Nil) === 1 :: 2 :: Nil)
   }
 
+  test("Selection Sort 2 elements") {
+    assert(selectionSort(List(1, 2)) === List(1, 2))
+    assert(selectionSort(List(2, 1)) === List(1, 2))
+
+  }
+
   test("Insert Sort 3 elements") {
     assert(insertSort(3 :: 2 :: 1 :: Nil) === 1 :: 2 :: 3 :: Nil)
     assert(insertSort(1 :: 3 :: 2 :: Nil) === 1 :: 2 :: 3 :: Nil)
+  }
+
+  test("Selection Sort 3 elements") {
+    assert(selectionSort(List(1, 2, 3)) === List(1, 2, 3))
+    assert(selectionSort(List(3, 2, 1)) === List(1, 2, 3))
+    assert(selectionSort(List(2, 1, 3)) === List(1, 2, 3))
+    assert(selectionSort(List(2, 3, 1)) === List(1, 2, 3))
+
   }
 
   test("Insert Sort N elements") {
