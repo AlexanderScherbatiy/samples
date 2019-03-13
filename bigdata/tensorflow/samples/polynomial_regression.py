c4891397e34f7ad150faaafbb3d35813a6ec3bd8
AlexanderScherbatiy <alexander.scherbatiy@yandex.com>
2019-03-13 13:55:39 +0300

    Split dataset into train and test

--- bigdata/tensorflow/samples/polynomial_regression.py
+++ bigdata/tensorflow/samples/polynomial_regression.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 learning_rate = 0.01
 training_epochs = 100
 
-observation_number = 101
+observation_number = 1001
 polynome_degree = 3
 
 
@@ -18,14 +18,26 @@ def polynomial(w, x):
     return y
 
 
+def shuffle_and_split_indices(size, split_rate):
+    split_index = int(split_rate * size)
+    indices = np.random.permutation(size)
+    return indices[:split_index], indices[split_index:]
+
+
 w_input = np.array([6.0, -20.0, 3.0, 2.0])
 
-x_train = np.linspace(-5, 5, observation_number)
-y_train = polynomial(w_input, x_train)
-plt.scatter(x_train, y_train, s=3)
+x_input = np.linspace(-5, 5, observation_number)
+y_input = polynomial(w_input, x_input)
+
+idx_train, idx_test = shuffle_and_split_indices(observation_number, 0.8)
+
+x_train, x_test = x_input[idx_train], x_input[idx_test]
+y_train, y_test = y_input[idx_train], y_input[idx_test]
+
+plt.scatter(x_input, y_input, s=3, marker='x')
 
 # Add noise
-y_train += np.random.normal(0, 2.0, x_train.shape)
+y_train += np.random.normal(0, 1.0, x_train.shape)
 plt.scatter(x_train, y_train, s=3)
 
 
@@ -43,7 +55,8 @@ Y = tf.placeholder(tf.float32)
 w = tf.Variable(tf.random_normal([polynome_degree + 1]), name='weights')
 Y_pred = model(w, X)
 
-cost = tf.reduce_sum(tf.pow(Y_pred - Y, 2)) / (observation_number - 1)
+cost = tf.reduce_sum(tf.square(Y_pred - Y)) / (observation_number - 1)
+
 train_op = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
 
 with tf.Session() as sess:
@@ -56,9 +69,14 @@ with tf.Session() as sess:
 
         if epoch % 20 == 0:
             w_val = sess.run(w)
-            print("epoch:", epoch, "learned w:", w_val)
+            cost_val = sess.run(cost, feed_dict={X: x_train, Y: y_train})
+            print("epoch:", epoch, "cost:", cost_val, "learned w:", w_val)
 
     w_val = sess.run(w)
+    train_cost_val = sess.run(cost, feed_dict={X: x_train, Y: y_train})
+    test_cost_val = sess.run(cost, feed_dict={X: x_test, Y: y_test})
+
+    print("train cost:", train_cost_val, 'test cost:', test_cost_val)
 
 print("real w:", w_input, "learned w:", w_val)
 
