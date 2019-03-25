c695eabc9b85822bec8601288d4fcbf1307264ea
Alexander Scherbatiy <alexander.scherbatiy@yandex.com>
2019-03-25 13:16:02 +0300

    Add answering sample to Rasa Chatbot

--- bigdata/datascience/chatbot/rasa/chatbot_sample.py
+++ bigdata/datascience/chatbot/rasa/chatbot_sample.py
@@ -1,12 +1,28 @@
 from rasa_nlu.model import Metadata, Interpreter
+from rasa_core.agent import Agent
+from rasa_core.interpreter import RasaNLUInterpreter
 
 interpreter = Interpreter.load('./models/nlu/default/chat')
 
 
-def ask_question(text):
-    print(interpreter.parse(text))
+def parse_question(question):
+    print('question:', question)
+    print('parse:', interpreter.parse(question))
 
 
-ask_question("Hey")
-ask_question("How many days in March")
-ask_question("Goodbye")
+parse_question("Hey")
+parse_question("How many days in March")
+parse_question("Goodbye")
+
+
+def ask_question(question):
+    print('question:', question)
+    print('answer:', agent.handle_message(question))
+
+
+rasaNLU = RasaNLUInterpreter("models/nlu/default/chat")
+agent = Agent.load("models/dialogue", interpreter=rasaNLU)
+
+ask_question('Hi')
+ask_question('How many days in January')
+ask_question('Bye')
--- bigdata/datascience/chatbot/rasa/chatbot_train.py
+++ bigdata/datascience/chatbot/rasa/chatbot_train.py
@@ -2,6 +2,10 @@ from rasa_nlu.training_data import load_data
 from rasa_nlu.model import Trainer
 from rasa_nlu import config
 
+import logging
+from rasa_core.agent import Agent
+from rasa_core.policies.memoization import MemoizationPolicy
+
 
 def train(data, config_file, model_dir):
     training_data = load_data(data)
@@ -13,3 +17,18 @@ def train(data, config_file, model_dir):
 # python3 -m spacy download en
 # You can now load the model via spacy.load('en')
 train('data/nlu_train.md', 'nlu_config.yml', 'models/nlu')
+
+
+def train_dialog(dialog_training_data_file, domain_file, path_to_model='models/dialogue'):
+    logging.basicConfig(level='INFO')
+    agent = Agent(domain_file,
+                  policies=[MemoizationPolicy(max_history=1)])
+    training_data = agent.load_data(dialog_training_data_file)
+    agent.train(training_data)
+
+    agent.persist(path_to_model)
+
+
+# Train
+# --------
+train_dialog('data/stories.md', 'domain.yml')
