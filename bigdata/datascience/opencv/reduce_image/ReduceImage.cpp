647c1fcf740b5fdd295d3317a58f239e4cf5abba
Alexander Scherbatiy <alexander.scherbatiy@yandex.com>
2019-04-16 13:09:15 +0300

    Use LUT to reduce colors in OpenCV sample

--- bigdata/datascience/opencv/reduce_image/ReduceImage.cpp
+++ bigdata/datascience/opencv/reduce_image/ReduceImage.cpp
@@ -2,36 +2,7 @@
 #include <opencv2/opencv.hpp>
 using namespace cv;
 
-Mat& ScanImageAndReduceC(Mat& I, const uchar* const table)
-{
-    // accept only char type matrices
-    CV_Assert(I.depth() == CV_8U);
-    int channels = I.channels();
-    int nRows = I.rows;
-    int nCols = I.cols * channels;
-
-    if (I.isContinuous())
-    {
-        nCols *= nRows;
-        nRows = 1;
-    }
-
-    int i,j;
-    uchar* p;
-
-    for( i = 0; i < nRows; ++i)
-    {
-        p = I.ptr<uchar>(i);
-        for ( j = 0; j < nCols; ++j)
-        {
-            p[j] = table[p[j]];
-        }
-    }
-
-    return I;
-}
-
-int main(int argc, char** argv )
+int main(int argc, char **argv)
 {
 
     if (argc != 2)
@@ -40,7 +11,7 @@ int main(int argc, char** argv )
         return -1;
     }
 
-    Mat image = imread( argv[1], 1 );
+    Mat image = imread(argv[1], 1);
 
     if (!image.data)
     {
@@ -48,16 +19,15 @@ int main(int argc, char** argv )
         return -1;
     }
 
+    int divideWith = 64;
 
-    int divideWith = 32;
+    Mat lookUpTable(1, 256, CV_8U);
+    uchar *p = lookUpTable.ptr();
+    for (int i = 0; i < 256; ++i)
+        p[i] = (uchar)(divideWith * (i / divideWith));
 
-    uchar table[256];
-    for (int i = 0; i < 256; ++i) {
-       table[i] = (uchar)(divideWith * (i/divideWith));
-    }
-
-
-    Mat reducedImage = ScanImageAndReduceC(image, table);
+    Mat reducedImage;
+    LUT(image, lookUpTable, reducedImage);
 
     namedWindow("Reduce Image", WINDOW_AUTOSIZE);
     imshow("Reduc Image", reducedImage);
