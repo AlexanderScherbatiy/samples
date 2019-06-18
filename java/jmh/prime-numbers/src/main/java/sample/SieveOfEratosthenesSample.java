0f23e1e86c4ae5eaabaae313c2827e9a279e02cb
Alexander Scherbatiy <alexander.scherbatiy@yandex.com>
2019-06-18 17:14:02 +0300

    Show in the sample only the last prime number in the elapsed time

--- java/jmh/prime-numbers/src/main/java/sample/SieveOfEratosthenesSample.java
+++ java/jmh/prime-numbers/src/main/java/sample/SieveOfEratosthenesSample.java
@@ -12,10 +12,22 @@ public class SieveOfEratosthenesSample {
         //  100:  541
         // 1000: 7919
 
-        int[] primes = SieveOfEratosthenesIntArray.findPrimes(1000000, 100);
-        System.out.printf("Primes:%n");
-        for (int i = 0; i < primes.length; i++) {
-            System.out.printf("prime[%d]=%d%n", i + 1, primes[i]);
+
+        int primesSize = 1000;
+
+        if (args.length > 0) {
+            try {
+                primesSize = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                System.out.printf("Provided primes size argument is not parsed as int: %s%n", args[0]);
+                System.exit(-1);
+            }
         }
+
+        long time = System.nanoTime();
+        int[] primes = SieveOfEratosthenesIntArray.findPrimes(primesSize, 10000);
+        time = System.nanoTime() - time;
+        System.out.printf("last prime[%d]: %d%n", primesSize, primes[primesSize - 1]);
+        System.out.printf("Elapsed time: %.3f s%n", time / 1e9);
     }
 }
