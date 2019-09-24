f426aceb6ab059c037dff51aff8dd7dc57f2ae81
Alexander Scherbatiy <stellarspot@gmail.com>
2019-09-24 13:33:04 +0300

    Add sample with custom IDs for JanusGraph

--- bigdata/graph/janusgraph/samples/src/main/java/sample/JanusGraphSample.java
+++ bigdata/graph/janusgraph/samples/src/main/java/sample/JanusGraphSample.java
@@ -1,47 +1,110 @@
 package sample;
 
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
+import org.apache.tinkerpop.gremlin.structure.T;
 import org.janusgraph.core.JanusGraph;
 import org.janusgraph.core.JanusGraphFactory;
 import org.janusgraph.core.JanusGraphTransaction;
+import org.janusgraph.graphdb.database.StandardJanusGraph;
+import org.janusgraph.graphdb.idmanagement.IDManager;
 
 public class JanusGraphSample {
 
-    public static JanusGraph getJanusGraph() {
-        return JanusGraphFactory.open("inmemory");
-
-
-//        String storageDirectory = "/tmp/samples/janusgraph";
-//        return JanusGraphFactory.build()
-//                .set("storage.backend", "berkeleyje")
-//                //.set("storage.backend", "inmemory")
-//                .set("storage.directory", String.format("%s/graph", storageDirectory))
-//                .set("query.force-index", true)
-//                .open();
-    }
-
+    private static final String LABEL = "SampleLabel";
+    private static final String KEY = "SampleKey";
+    private static final String VALUE = "SampleValue";
+    private static final int N = 100;
 
     public static void main(String[] args) {
-        System.out.printf("Hello, JanusGraph!%n");
+        addVerticesWithGeneratedIds();
+        addVerticesWithCustomIds();
+    }
 
+    private static JanusGraph getJanusGraph(boolean customIds) {
+        JanusGraphFactory.Builder builder = JanusGraphFactory
+                .build()
+                //.set("query.force-index", true)
+                .set("storage.backend", "inmemory");
 
-        String label = "SampleLabel";
-        String key = "sampleKey";
-        String value = "sample_value_";
+        if (customIds) {
+            builder = builder.set("graph.set-vertex-id", "true");
+        }
 
-        int N = 100;
+        return builder.open();
+    }
 
-        try (JanusGraph graph = getJanusGraph();
-             JanusGraphTransaction tx = graph.newTransaction()) {
-            long time = System.currentTimeMillis();
-            System.out.printf("N: %d%n", N);
+    static void clearGraph(GraphTraversalSource g) {
+        g.V().drop().iterate();
+    }
+
+    static void clearGraph(JanusGraph graph) {
+        try (JanusGraphTransaction tx = graph.newTransaction()) {
             GraphTraversalSource g = tx.traversal();
-
-            for (int i = 0; i < N; i++) {
-                g.addV(label).property(key, value + i).next();
-            }
+            g.V().drop().iterate();
             tx.commit();
-            System.out.printf("elapsed time: %d(ms)%n", System.currentTimeMillis() - time);
+        }
+    }
+
+    static long verticesCount(JanusGraph graph) {
+        try (JanusGraphTransaction tx = graph.newTransaction()) {
+            GraphTraversalSource g = tx.traversal();
+            return g.V().count().next();
+        }
+    }
+
+    public static void addVerticesWithGeneratedIds() {
+
+        System.out.printf("Add vertices with generated ids%n");
+
+        try (JanusGraph graph = getJanusGraph(false)) {
+
+            clearGraph(graph);
+
+            try (JanusGraphTransaction tx = graph.newTransaction()) {
+                GraphTraversalSource g = tx.traversal();
+
+                System.out.printf("N: %d%n", N);
+                long time = System.currentTimeMillis();
+                for (int i = 1; i <= N; i++) {
+                    String value = String.format("%s-%d", VALUE, i);
+                    g.addV(LABEL).property(KEY, value).next();
+                }
+                tx.commit();
+                System.out.printf("elapsed time: %d(ms)%n", System.currentTimeMillis() - time);
+            }
+
+            long vertices = verticesCount(graph);
+            System.out.printf("added vertices: %d%n", vertices);
+        }
+    }
+
+    public static void addVerticesWithCustomIds() {
+
+        System.out.printf("Add vertices with custom ids%n");
+
+        try (JanusGraph graph = getJanusGraph(true)) {
+
+            clearGraph(graph);
+
+            IDManager idManager = ((StandardJanusGraph) graph).getIDManager();
+
+            try (JanusGraphTransaction tx = graph.newTransaction()) {
+                GraphTraversalSource g = tx.traversal();
+
+                System.out.printf("N: %d%n", N);
+                long time = System.currentTimeMillis();
+                for (int i = 1; i <= N; i++) {
+                    long id = idManager.toVertexId(i);
+                    String value = String.format("%s-%d", VALUE, i);
+                    g.addV(LABEL).property(T.id, id).property(KEY, value).next();
+
+                }
+                tx.commit();
+                System.out.printf("elapsed time: %d(ms)%n", System.currentTimeMillis() - time);
+            }
+
+            long vertices = verticesCount(graph);
+            System.out.printf("added vertices: %d%n", vertices);
         }
     }
 }
