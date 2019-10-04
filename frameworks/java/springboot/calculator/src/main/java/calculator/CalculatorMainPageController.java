d9af45f2bd1f104122b44bcb88b9aec01283fdb3
Alexander Scherbatiy <stellarspot@gmail.com>
2019-10-04 12:15:03 +0300

    Return list of calculators from controller

--- frameworks/java/springboot/calculator/src/main/java/calculator/CalculatorMainPageController.java
+++ frameworks/java/springboot/calculator/src/main/java/calculator/CalculatorMainPageController.java
@@ -1,13 +1,22 @@
 package calculator;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
+@RequestMapping("/")
 public class CalculatorMainPageController {
 
-    @RequestMapping("/")
-    public String index() {
+    @GetMapping
+    public String index(Model model) {
+
+        List<String> refs = Arrays.asList("classic", "scientific");
+        model.addAttribute("refs", refs);
         return "index";
     }
 }
