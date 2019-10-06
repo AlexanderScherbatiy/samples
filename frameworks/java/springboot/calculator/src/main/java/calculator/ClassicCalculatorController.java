52121ef26e8242b6b27934e23a225d1d9acb2c3d
Alexander Scherbatiy <stellarspot@gmail.com>
2019-10-06 22:23:52 +0300

    Print Classis calculator results

--- frameworks/java/springboot/calculator/src/main/java/calculator/ClassicCalculatorController.java
+++ frameworks/java/springboot/calculator/src/main/java/calculator/ClassicCalculatorController.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,21 +8,27 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("/classic")
 public class ClassicCalculatorController {
 
+    List<String> results = new ArrayList<>();
+
     @GetMapping
     public String calculateForm(Model model) {
-
         model.addAttribute("expression", new Expression());
+        model.addAttribute("results", results);
         return "classic";
     }
 
     @PostMapping
-    public String calculateSubmit(@ModelAttribute Expression expression) {
-        System.out.printf("expression: %s%n", expression);
+    public String calculateSubmit(@ModelAttribute Expression expression, Model model) {
+        results.add(Double.toString(expression.calculate()));
+        model.addAttribute("expression", new Expression());
+        model.addAttribute("results", results);
         return "classic";
     }
-
 }
--- frameworks/java/springboot/calculator/src/main/java/calculator/Expression.java
+++ frameworks/java/springboot/calculator/src/main/java/calculator/Expression.java
@@ -8,4 +8,14 @@ public class Expression {
     private double a;
     private double b;
     private String operation;
+
+    public enum Operation {
+        PLUS,
+        MINUS,
+        MULTIPLY
+    }
+
+    public double calculate() {
+        return 42;
+    }
 }
