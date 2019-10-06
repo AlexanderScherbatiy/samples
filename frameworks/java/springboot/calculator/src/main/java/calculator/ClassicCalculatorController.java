6e726fdd7fc0a56418d5c07e051890b8f2bd1854
Alexander Scherbatiy <stellarspot@gmail.com>
2019-10-06 08:42:17 +0300

    Add Classic calculator submission form

--- frameworks/java/springboot/calculator/src/main/java/calculator/ClassicCalculatorController.java
+++ frameworks/java/springboot/calculator/src/main/java/calculator/ClassicCalculatorController.java
@@ -1,7 +1,10 @@
 package calculator;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,7 +12,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ClassicCalculatorController {
 
     @GetMapping
-    public String calculate() {
+    public String calculateForm(Model model) {
+
+        model.addAttribute("expression", new Expression());
+        return "classic";
+    }
+
+    @PostMapping
+    public String calculateSubmit(@ModelAttribute Expression expression) {
+        System.out.printf("expression: %s%n", expression);
         return "classic";
     }
 
