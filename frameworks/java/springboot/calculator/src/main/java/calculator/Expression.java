c7166e2a1300d9af27275ad0f0717cfd7ab72f04
Alexander Scherbatiy <stellarspot@gmail.com>
2019-10-06 14:38:30 +0300

    Use Lombok Data annotation

--- frameworks/java/springboot/calculator/src/main/java/calculator/Expression.java
+++ frameworks/java/springboot/calculator/src/main/java/calculator/Expression.java
@@ -1,32 +1,11 @@
 package calculator;
 
+import lombok.Data;
+
+@Data
 public class Expression {
 
     private double a;
     private double b;
     private String operation;
-
-    public double getA() {
-        return a;
-    }
-
-    public void setA(double a) {
-        this.a = a;
-    }
-
-    public double getB() {
-        return b;
-    }
-
-    public void setB(double b) {
-        this.b = b;
-    }
-
-    public String getOperation() {
-        return operation;
-    }
-
-    public void setOperation(String operation) {
-        this.operation = operation;
-    }
 }
