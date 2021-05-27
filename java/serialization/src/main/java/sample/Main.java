9f01cf786bdf1c7e81bda309a95f5fb642943424
AlexanderScherbatiy <alexander.scherbatiy@bell-sw.com>
2021-05-27 13:12:57 +0300

    Add Kryo serialization sample

--- java/serialization/src/main/java/sample/Main.java
+++ java/serialization/src/main/java/sample/Main.java
@@ -1,5 +1,11 @@
 package sample;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
 import serialization.ByteBufferSerializer;
 import serialization.Serializer;
 
@@ -7,16 +13,33 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        Hello hello = new Hello("Alice");
+        Person alice = new Person("Alice", 28);
+
+        // Java ByteBufferSerializer
         Serializer byteBufferSerializer = new ByteBufferSerializer();
-        byte[] bytes = byteBufferSerializer.serialize(hello);
-        Hello deserializedHello = (Hello) byteBufferSerializer.deserialize(bytes);
+        byte[] bytes = byteBufferSerializer.serialize(alice);
+        Person deserializedAlice = (Person) byteBufferSerializer.deserialize(bytes);
+        System.out.printf("java serializer buffer size: %d, object: %s%n", bytes.length, deserializedAlice);
 
-        System.out.printf("byte buffer size: %d, hello string: %s%n", bytes.length, deserializedHello.hello());
+        // Kryo
+        Kryo kryo = new Kryo();
+        kryo.register(Person.class);
 
+        ByteArrayOutputStream kryoByteArray = new ByteArrayOutputStream();
+        try (Output output = new Output(kryoByteArray)) {
+            kryo.writeObject(output, alice);
+        }
+
+        bytes = kryoByteArray.toByteArray();
+        try (Input input = new Input(new ByteArrayInputStream(bytes))) {
+            Person kryoAlice = kryo.readObject(input, Person.class);
+            System.out.printf("kryo buffer size: %d, object: %s%n", kryoByteArray.toByteArray().length, kryoAlice);
+        }
+
+        // ProtoBuf
         ProtoHello.Hello protoHello = ProtoHello.Hello
                 .newBuilder()
-                .setName("Bob")
+                .setName("Alice")
                 .build();
 
         byte[] protobufBytes = protoHello.toByteArray();
---
+++ java/serialization/src/main/java/sample/Person.java
@@ -0,0 +1,52 @@
+package sample;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Person implements Serializable {
+
+    private String name;
+    private int age;
+
+    private Person() {
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person that = (Person) o;
+        return this.age == that.age && Objects.equals(this.name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Person(name: %s, age: %d)", name, age);
+    }
+}
